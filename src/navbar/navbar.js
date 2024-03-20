18eb294a155bec3f5016c659eaa96cbf29b63365
MEEN15361 <90585323+MEEN15361@users.noreply.github.com>
2024-03-20 15:13:37 +0700

    Navbar with mock data

--- src/navbar/navbar.js
+++ src/navbar/navbar.js
@@ -4,7 +4,23 @@ import './navbar.scss';
 const Navbar = () => {
   return (
     <div className='navbarContainer'>
-      
+      <div className='logo'>
+        <img id='logo'
+          src='https://wsapp01.winnersoft.co.th/Runtime/Image.ashx?ImID=224'
+          alt='' />
+      </div>
+      <div className='systemName'>
+        <div id='system_name'>Winnersoft Portal</div>
+      </div>
+      <div className='userLogin'>
+        <div className='userLoginBox'>
+          <img id='user_icon'
+            src='https://wsapp01.winnersoft.co.th/Runtime/Image.ashx?ImID=189'
+            alt='' />
+          <div id='login_name'>Wasan Senkha(WINNERSOFT\Wasan.s)</div>
+          <div id='logout_button'>Logout</div>
+        </div>
+      </div>
     </div>
   );
 };
